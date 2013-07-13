90220417880501f5c47e0460ed702eca150784ad
Łukasz Mrozek <morskk@gmail.com>
2013-07-13 22:50:09 +0200

    Added attribute usage for markers.

--- Journeys.Domain/Markers/EntityAttribute.cs
+++ Journeys.Domain/Markers/EntityAttribute.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace Journeys.Domain.Markers
 {
+    [AttributeUsage(AttributeTargets.Class)]
     internal class EntityAttribute : Attribute
     {
     }
--- Journeys.Domain/Markers/ValueObjectAttribute.cs
+++ Journeys.Domain/Markers/ValueObjectAttribute.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace Journeys.Domain.Markers
 {
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
     internal class ValueObjectAttribute : Attribute
     {
     }
