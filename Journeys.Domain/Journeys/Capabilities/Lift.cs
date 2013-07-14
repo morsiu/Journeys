41921e87254fdb2f25c897bcd9efb3f4a7db710c
Łukasz Mrozek <morskk@gmail.com>
2013-07-14 02:26:15 +0200

    Moved Lift to capabilities layer.

---
+++ Journeys.Domain/Journeys/Capabilities/Lift.cs
@@ -0,0 +1,25 @@
+﻿using Journeys.Domain.Identities;
+using Journeys.Domain.Journeys.Capabilities;
+using Journeys.Domain.Markers;
+using Journeys.Domain.People;
+
+namespace Journeys.Domain.Journeys.Capabilities
+{
+    [ValueObject]
+    public class Lift
+    {
+        private Id<Person> _personId;
+        private Distance _distance;
+
+        public Lift(Id<Person> personId, Distance distance)
+        {
+            _personId = personId;
+            _distance = distance;
+        }
+
+        public bool EqualsByPerson(Id<Person> personId)
+        {
+            return _personId == personId;
+        }
+    }
+}
