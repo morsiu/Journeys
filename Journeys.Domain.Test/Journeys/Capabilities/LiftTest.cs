ad0ed2d9fc7936d9eebeb5e9c4b1f803f71065c7
Łukasz Mrozek <morskk@gmail.com>
2013-07-14 13:18:58 +0200

    Added tests for lift.

---
+++ Journeys.Domain.Test/Journeys/Capabilities/LiftTest.cs
@@ -0,0 +1,32 @@
+﻿using Journeys.Domain.Infrastructure;
+using Journeys.Domain.Journeys.Capabilities;
+using Journeys.Domain.People;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Journeys.Domain.Test.Journeys.Capabilities
+{
+    [TestClass]
+    public class LiftTest
+    {
+        [TestMethod]
+        public void LiftWithPersonIdShouldBeEqualByPersonToThatId()
+        {
+            var lift = new Lift(new Id<Person>(1), new Distance(1m, DistanceUnit.Kilometer));
+
+            Assert.IsTrue(lift.EqualsByPerson(new Id<Person>(1)));
+        }
+
+        [TestMethod]
+        public void LiftWithPersonIdShouldNotBeEqualByPersonToOtherId()
+        {
+            var lift = new Lift(new Id<Person>(1), new Distance(1m, DistanceUnit.Kilometer));
+
+            Assert.IsFalse(lift.EqualsByPerson(new Id<Person>(2)));
+        }
+    }
+}
