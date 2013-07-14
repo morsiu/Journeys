697586027bbcffd0c4738088a467bb91b5c18b34
Łukasz Mrozek <morskk@gmail.com>
2013-07-14 10:57:16 +0200

    Markers should be internal

--- Journeys.Domain/Infrastructure/Markers/ServiceAttribute.cs
+++ Journeys.Domain/Infrastructure/Markers/ServiceAttribute.cs
@@ -3,7 +3,7 @@
 namespace Journeys.Domain.Infrastructure.Markers
 {
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface)]
-    public class ServiceAttribute : Attribute
+    internal class ServiceAttribute : Attribute
     {
     }
 }
