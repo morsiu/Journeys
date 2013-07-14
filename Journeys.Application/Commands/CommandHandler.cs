e97607cc5b3d7ebb50021dd4d334e54592964557
Łukasz Mrozek <morskk@gmail.com>
2013-07-14 18:39:27 +0200

    Added command handling.

---
+++ Journeys.Application/Commands/CommandHandler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Journeys.Application.Commands
+{
+    public delegate void CommandHandler<TCommand>(TCommand command);
+}
---
+++ Journeys.Application/Commands/CommandProcessor.cs
@@ -0,0 +1,36 @@
+﻿using Journeys.Application.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Journeys.Application.Commands
+{
+    internal class CommandProcessor
+    {
+        private readonly Dictionary<Type, Action<object>> _handlers = new Dictionary<Type, Action<object>>();
+
+        public void RegisterHandler<TCommand>(CommandHandler<TCommand> handler)
+        {
+            var commandType = typeof(TCommand);
+            Action<object> untypedHandler = command => handler((TCommand)command);
+            _handlers[commandType] = untypedHandler;
+        }
+
+        public void Handle(object command)
+        {
+            if (command == null) throw new ArgumentNullException("command");
+            var untypedHandler = GetHandler(command);
+            untypedHandler(command);
+        }
+
+        private Action<object> GetHandler(object command)
+        {
+            var commandType = command.GetType();
+            if (!_handlers.ContainsKey(commandType)) throw new ArgumentException(string.Format(FailureMessages.NoHandlerRegisteredForCommandOfType, commandType));
+            var untypedHandler = _handlers[commandType];
+            return untypedHandler;
+        }
+    }
+}
