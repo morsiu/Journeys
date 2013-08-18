a97ad23c740eef01ff5fd23e6876bb79a0248448
Łukasz Mrozek <morskk@gmail.com>
2013-08-18 17:21:56 +0200

    Removed files.

--- Journeys.Queries/GetAllJourneysWithLiftsQuery.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-using Journeys.Queries.Dtos;
-
-namespace Journeys.Queries
-{
-    public class GetAllJourneysWithLiftsQuery : IQuery<IEnumerable<JourneyWithLift>>
-    {
-    }
-}
