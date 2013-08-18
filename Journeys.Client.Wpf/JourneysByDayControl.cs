1333aa0379854c2f5f8fb64b557f3d2da43e0974
Łukasz Mrozek <morskk@gmail.com>
2013-08-18 15:00:29 +0200

    Refresh button handling was done in wrong control.

--- Journeys.Client.Wpf/JourneysByDayControl.cs
+++ Journeys.Client.Wpf/JourneysByDayControl.cs
@@ -1,29 +1,8 @@
-﻿using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Controls.Primitives;
-using System.Windows.Input;
+﻿using System.Windows.Controls;
 
 namespace Journeys.Client.Wpf
 {
     internal class JourneysByDayControl : Control
     {
-        public override void OnApplyTemplate()
-        {
-            base.OnApplyTemplate();
-            var refreshButton = Template.FindName("RefreshButton", this) as ButtonBase;
-            if (refreshButton != null)
-            {
-                refreshButton.Click += OnRefreshButtonClick;
-            }
-        }
-
-        private void OnRefreshButtonClick(object sender, RoutedEventArgs e)
-        {
-            var refreshCommand = (ICommand)(DataContext as dynamic).RefreshCommand;
-            if (refreshCommand != null)
-            {
-                refreshCommand.Execute(null);
-            }
-        }
     }
 }
--- Journeys.Client.Wpf/JourneysByPassengerThenDayControl.cs
+++ Journeys.Client.Wpf/JourneysByPassengerThenDayControl.cs
@@ -1,8 +1,29 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
 
 namespace Journeys.Client.Wpf
 {
     internal class JourneysByPassengerThenDayControl : Control
     {
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            var refreshButton = Template.FindName("RefreshButton", this) as ButtonBase;
+            if (refreshButton != null)
+            {
+                refreshButton.Click += OnRefreshButtonClick;
+            }
+        }
+
+        private void OnRefreshButtonClick(object sender, RoutedEventArgs e)
+        {
+            var refreshCommand = (ICommand)(DataContext as dynamic).RefreshCommand;
+            if (refreshCommand != null)
+            {
+                refreshCommand.Execute(null);
+            }
+        }
     }
 }
