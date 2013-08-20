5a073374d1ab6c5538fc9c67a9320bfcbab11ccb
Łukasz Mrozek <morskk@gmail.com>
2013-08-20 20:42:24 +0200

    Added Transaction class that runs code inside transaction.

---
+++ Journeys.Transactions/Transaction.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Journeys.Transactions
+{
+    public class Transaction
+    {
+        private HashSet<ITransactable> _transactables = new HashSet<ITransactable>();
+
+        public TObject Add<TObject>(IProvideTransacted<TObject> @object)
+        {
+            var transactedObject = @object.Lift();
+            _transactables.Add(transactedObject);
+            return transactedObject.Object;
+        }
+
+        public void Run(Action action)
+        {
+            try
+            {
+                action();
+            }
+            catch
+            {
+                foreach (var transactable in _transactables)
+                {
+                    transactable.Abort();
+                }
+                throw;
+            }
+            foreach (var transactable in _transactables)
+            {
+                transactable.Commit();
+            }
+        }
+
+        public void Run<T1>(Action<T1> action, T1 parameter1)
+        {
+            Run(() => action(parameter1));
+        }
+    }
+}
