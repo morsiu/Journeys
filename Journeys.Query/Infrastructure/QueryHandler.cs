7b45c3f66ee5ddc83f591257a5b1ddad7ceb8d81
Łukasz Mrozek <morskk@gmail.com>
2013-08-27 22:15:16 +0200

    Removed old files.

--- Journeys.Query/Infrastructure/QueryHandler.cs
+++
@@ -1,7 +0,0 @@
-﻿using Journeys.Queries;
-
-namespace Journeys.Query.Infrastructure
-{
-    internal delegate TResult QueryHandler<TQuery, TResult>(TQuery query)
-        where TQuery : IQuery<TResult>;
-}
