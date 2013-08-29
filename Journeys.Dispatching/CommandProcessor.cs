da4623cd2bccc719cff7e45cfa3c89a35cbfc20b
Łukasz Mrozek <morskk@gmail.com>
2013-08-29 20:46:23 +0200

    Replaced Dictionary with HandlerRegistry in CommandProcessor.

--- Journeys.Dispatching/CommandProcessor.cs
+++ Journeys.Dispatching/CommandProcessor.cs
@@ -7,28 +7,26 @@ namespace Journeys.Dispatching
     public class CommandProcessor
     {
         private delegate void UntypedCommandHandler(object command);
-        private readonly Dictionary<Type, UntypedCommandHandler> _handlers = new Dictionary<Type, UntypedCommandHandler>();
+        private readonly HandlerRegistry _handlers = new HandlerRegistry();
 
         public void SetHandler<TCommand>(CommandHandler<TCommand> handler)
         {
             var commandType = typeof(TCommand);
-            UntypedCommandHandler untypedHandler = command => handler((TCommand)command);
-            _handlers[commandType] = untypedHandler;
+            _handlers.Set(commandType, command => { handler((TCommand)command); return null; });
         }
-
+        
         public void Handle(object command)
         {
-            if (command == null) throw new ArgumentNullException("command");
-            var untypedHandler = GetHandler(command);
-            untypedHandler(command);
-        }
-
-        private UntypedCommandHandler GetHandler(object command)
-        {
             var commandType = command.GetType();
-            if (!_handlers.ContainsKey(commandType)) throw new InvalidOperationException(string.Format(FailureMessages.NoHandlerRegisteredForCommandOfType, commandType));
-            var untypedHandler = _handlers[commandType];
-            return untypedHandler;
+            Func<object, object> handler;
+            if (_handlers.Retrieve(commandType, out handler))
+            {
+                handler(command);
+            }
+            else
+            {
+                throw new InvalidOperationException(string.Format(FailureMessages.NoHandlerRegisteredForCommandOfType, commandType));
+            }
         }
     }
 }
