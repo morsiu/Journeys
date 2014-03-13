4f60d22a2f70f43c812786a9182853d18e42eb0f
Łukasz Mrozek <morskk@gmail.com>
2014-03-13 18:52:37 +0100

    Added tests for Person.

---
+++ Journeys.Domain.People.Test/PersonTest.cs
@@ -0,0 +1,49 @@
+﻿using Journeys.Common;
+using Journeys.Domain.Infrastructure.Exceptions;
+using Journeys.Domain.Test.Infrastructure;
+using Journeys.Events;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Journeys.Domain.People.Test
+{
+    [TestClass]
+    public class PersonTest
+    {
+        private static readonly string PersonName = "PersonName";
+        private static readonly IId PersonId = new Id(0);
+        private EventBusMock _eventBus;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _eventBus = new EventBusMock();
+        }
+
+        [TestMethod]
+        public void ShouldPublishEventWhenCreatingPerson()
+        {
+            var eventMatcher = _eventBus.Listen(() =>
+            {
+                new Person(PersonId, PersonName, _eventBus.Object);
+            });
+
+            eventMatcher.AssertReceivedOneEvent<PersonCreatedEvent>(
+                evt => evt.PersonId.Equals(PersonId) &&
+                       evt.PersonName == PersonName);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvariantViolationException))]
+        public void ShouldReportInvariantViolationWhenCreatingPersonWithEmptyName()
+        {
+            new Person(PersonId, string.Empty, _eventBus.Object);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvariantViolationException))]
+        public void ShouldReportInvariantViolationWhenCreatingPersonWithNullName()
+        {
+            new Person(PersonId, null, _eventBus.Object);
+        }
+    }
+}
