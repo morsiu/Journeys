58776eebcfb36c42490223b59ebcf7d5a4675115
Łukasz Mrozek <morskk@gmail.com>
2014-03-16 19:05:31 +0100

    Added money value object to expenses domain.

---
+++ Journeys.Domain.Expenses.Test/Capabilities/MoneyTest.cs
@@ -0,0 +1,56 @@
+﻿using Journeys.Domain.Expenses.Capabilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Journeys.Domain.Expenses.Test.Capabilities
+{
+    [TestClass]
+    public class MoneyTest
+    {
+        [TestMethod]
+        public void ShouldHaveAmountGivenAtConstruction()
+        {
+            var money = new Money(10m);
+
+            Assert.AreEqual(10, money.Amount);
+        }
+
+        [TestMethod]
+        public void ShouldHaveZeroAmountAfterDefaultConstruction()
+        {
+            var money = new Money();
+
+            Assert.AreEqual(0, money.Amount);
+        }
+
+        [TestMethod]
+        public void AddingMoneyShouldResultInMoneyWithAmountBeingSumOfAddentsAmounts()
+        {
+            var a = new Money(10m);
+            var b = new Money(20m);
+
+            var c = a + b;
+
+            Assert.AreEqual(30, c.Amount);
+        }
+
+        [TestMethod]
+        public void MultiplyingMoneyByNumberShouldResultInMoneyWithAmountMultipliedByThatNumber()
+        {
+            var a = new Money(10m);
+
+            var c = a * 3;
+
+            Assert.AreEqual(30, c.Amount);
+        }
+
+        [TestMethod]
+        public void MultiplyingNumberByMoneyShouldResultInMoneyWithAmountMultipliedByThatNumber()
+        {
+            var a = new Money(10m);
+
+            var c = 3 * a;
+
+            Assert.AreEqual(30, c.Amount);
+        }
+    }
+}
