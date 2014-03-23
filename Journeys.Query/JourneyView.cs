7b04f11e45737fafcd3da7276cfcc43d5171329c
Łukasz Mrozek <morskk@gmail.com>
2014-03-23 12:05:02 +0100

    Implemented GetJourneysInPeriodQuery handling.

--- Journeys.Query/JourneyView.cs
+++ Journeys.Query/JourneyView.cs
@@ -1,24 +1,57 @@
-﻿using Journeys.Events;
+﻿using Journeys.Common;
+using Journeys.Events;
 using Journeys.Queries;
 using Journeys.Queries.Dtos;
+using Journeys.Query.Infrastructure.Views;
 using System.Collections.Generic;
 using System.Linq;
+using Period = Journeys.Query.Infrastructure.Period;
 
 namespace Journeys.Query
 {
     internal class JourneyView
     {
-        public void Update(JourneyCreatedEvent obj)
+        private readonly ValueSet<IId, JourneyCreatedEvent> _journeys;
+        private readonly ValueMultiSet<IId, LiftAddedEvent> _lifts;
+
+        public JourneyView()
         {
+            _journeys = new ValueSet<IId, JourneyCreatedEvent>(evt => evt.JourneyId);
+            _lifts = new ValueMultiSet<IId, LiftAddedEvent>(evt => evt.JourneyId);
         }
 
-        public void Update(LiftAddedEvent obj)
+        public void Update(JourneyCreatedEvent @event)
         {
+            _journeys.Add(@event);
+        }
+
+        public void Update(LiftAddedEvent @event)
+        {
+            _lifts.Add(@event);
         }
 
         public IEnumerable<Journey> Execute(GetJourneysInPeriodQuery query)
         {
-            return Enumerable.Empty<Journey>();
+            var period = new Period(query.Period.Start, query.Period.End);
+            var journeysInPeriod = _journeys.Retrieve().Where(j => period.Contains(j.DateOfOccurrence));
+            return journeysInPeriod.Select(ToDto).ToList();
+        }
+
+        private Journey ToDto(JourneyCreatedEvent journey)
+        {
+            var lifts = _lifts.GetOrDefault(journey.JourneyId);
+            return new Journeys.Queries.Dtos.Journey(
+                journey.JourneyId,
+                journey.DateOfOccurrence,
+                journey.RouteDistance,
+                lifts.Select(ToDto).ToList());
+        }
+
+        private Lift ToDto(LiftAddedEvent lift)
+        {
+            return new Journeys.Queries.Dtos.Lift(
+                lift.PersonId,
+                lift.LiftDistance);
         }
     }
 }
