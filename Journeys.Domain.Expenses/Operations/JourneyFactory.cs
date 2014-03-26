73817e94df4ea6e44c7c3343afedfc9a7bbe81a5
Łukasz Mrozek <morskk@gmail.com>
2014-03-26 22:08:43 +0100

    Added missing JourneyFactory file

---
+++ Journeys.Domain.Expenses/Operations/JourneyFactory.cs
@@ -0,0 +1,50 @@
+﻿using Journeys.Common;
+using Journeys.Domain.Expenses.Capabilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Journeys.Domain.Expenses.Operations
+{
+    public sealed class JourneyFactory
+    {
+        public JourneyBuilder Create(IId journeyId, decimal distance)
+        {
+            return new JourneyBuilder(journeyId, distance);
+        }
+
+        public class JourneyBuilder
+        {
+            private readonly IId _journeyId;
+            private readonly Point _distance;
+            private readonly Dictionary<IId, decimal> _liftDistancesByPassengerId = new Dictionary<IId,decimal>();
+
+            public JourneyBuilder(IId journeyId, decimal distance)
+            {
+                _journeyId = journeyId;
+                _distance = new Point(distance);
+            }
+
+            public void AddLift(IId passengerId, decimal distance)
+            {
+                if (_liftDistancesByPassengerId.ContainsKey(passengerId))
+                {
+                    throw new ArgumentException(Messages.JourneyAlreadyContainsLiftForThatPerson, "passengerId");
+                }
+                _liftDistancesByPassengerId[passengerId] = distance;
+            }
+
+            public Journey Build()
+            {
+                var lifts = _liftDistancesByPassengerId.Select(BuildLift);
+                return new Journey(_journeyId, _distance, lifts);
+            }
+
+            private Lift BuildLift(KeyValuePair<IId, decimal> passengerIdAndDistance)
+            {
+                var liftDistance = new Distance(new Point(), new Point(passengerIdAndDistance.Value));
+                return new Lift(passengerIdAndDistance.Key, liftDistance);
+            }
+        }
+    }
+}
