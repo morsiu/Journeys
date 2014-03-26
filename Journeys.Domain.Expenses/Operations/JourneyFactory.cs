a1509d0719af1441bbe141f88d7b1e8ce4435e3d
Łukasz Mrozek <morskk@gmail.com>
2014-03-26 22:12:12 +0100

    Moved out JourneyBuilder from JourneyFactory.

--- Journeys.Domain.Expenses/Operations/JourneyFactory.cs
+++ Journeys.Domain.Expenses/Operations/JourneyFactory.cs
@@ -12,39 +12,5 @@ namespace Journeys.Domain.Expenses.Operations
         {
             return new JourneyBuilder(journeyId, distance);
         }
-
-        public class JourneyBuilder
-        {
-            private readonly IId _journeyId;
-            private readonly Point _distance;
-            private readonly Dictionary<IId, decimal> _liftDistancesByPassengerId = new Dictionary<IId,decimal>();
-
-            public JourneyBuilder(IId journeyId, decimal distance)
-            {
-                _journeyId = journeyId;
-                _distance = new Point(distance);
-            }
-
-            public void AddLift(IId passengerId, decimal distance)
-            {
-                if (_liftDistancesByPassengerId.ContainsKey(passengerId))
-                {
-                    throw new ArgumentException(Messages.JourneyAlreadyContainsLiftForThatPerson, "passengerId");
-                }
-                _liftDistancesByPassengerId[passengerId] = distance;
-            }
-
-            public Journey Build()
-            {
-                var lifts = _liftDistancesByPassengerId.Select(BuildLift);
-                return new Journey(_journeyId, _distance, lifts);
-            }
-
-            private Lift BuildLift(KeyValuePair<IId, decimal> passengerIdAndDistance)
-            {
-                var liftDistance = new Distance(new Point(), new Point(passengerIdAndDistance.Value));
-                return new Lift(passengerIdAndDistance.Key, liftDistance);
-            }
-        }
     }
 }
