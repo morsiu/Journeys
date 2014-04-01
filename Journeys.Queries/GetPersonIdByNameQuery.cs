4aeac8842e3ded4592f1ee76411c8e7132ede7de
Łukasz Mrozek <morskk@gmail.com>
2014-04-01 21:13:44 +0200

    Added missing DataMember on dto.

--- Journeys.Queries/GetPersonIdByNameQuery.cs
+++ Journeys.Queries/GetPersonIdByNameQuery.cs
@@ -6,6 +6,7 @@ namespace Journeys.Queries
     [DataContract]
     public class GetPersonIdByNameQuery : IQuery<IId>
     {
+        [DataMember]
         public string PersonName { get; private set; }
 
         public GetPersonIdByNameQuery(string personName)
