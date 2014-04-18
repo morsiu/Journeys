2b912b8c988b26eff9e099b84afa690fbdb48e09
Łukasz Mrozek <morskk@gmail.com>
2014-04-18 21:01:49 +0200

    Update month name when month changes

--- Journeys.Client.Web/site/components/calendar/calendar-directive.js
+++ Journeys.Client.Web/site/components/calendar/calendar-directive.js
@@ -8,10 +8,15 @@
         transclude: true,
         templateUrl: 'components/calendar/calendar-template.html',
         controller: ['$scope', 'calendarService', function ($scope, calendarService) {
-            $scope.dayNames = calendarService.shortDayNames;
-            $scope.monthNames = calendarService.longMonthNames;
             $scope.columns = [0, 1, 2, 3, 4, 5, 6];
-            $scope.rows = [0, 1, 2, 3, 4, 5, 6]
+            $scope.rows = [0, 1, 2, 3, 4];
+
+            $scope.dayNames = calendarService.shortDayNames;
+
+            var monthNames = calendarService.longMonthNames;
+            $scope.$watch('month', function () {
+                $scope.monthName = monthNames[$scope.month] || '';
+            });
         }]
     };
 };
