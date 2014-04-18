618c533b1eb7114833f44b67f463fdc47f6e05ae
Łukasz Mrozek <morskk@gmail.com>
2014-04-19 00:06:18 +0200

    Fixed scopes of transcluded elements inheriting from ngRepeat isolated scope by compiling the table contents

--- Journeys.Client.Web/site/components/calendar/calendar-directive.js
+++ Journeys.Client.Web/site/components/calendar/calendar-directive.js
@@ -7,10 +7,40 @@
         replace: true,
         transclude: true,
         templateUrl: 'components/calendar/calendar-template.html',
+        compile: function(tElement) {
+            var columnCount = 7;
+            var rowCount = 5;
+            var table = tElement[0];
+            var tableBody = table.tBodies[0];
+            for (var rowIdx = 0; rowIdx < rowCount; ++rowIdx) {
+                var row = tableBody.insertRow();
+                for (var columnIdx = 0; columnIdx < columnCount; ++columnIdx) {
+                    var cell = row.insertCell();
+                };
+            };
+            return {
+                pre: function (scope, iElement, iAttrs, controller, transcludeFn) {
+                    var table = iElement[0];
+                    var tableBody = table.tBodies[0];
+                    for (var rowIdx = 0; rowIdx < rowCount; ++rowIdx) {
+                        var row = tableBody.rows[rowIdx];
+                        for (var columnIdx = 0; columnIdx < columnCount; ++columnIdx) {
+                            var cell = row.cells[columnIdx];
+                            transcludeFn(function (contents, contentsScope) {
+                                contentsScope.$row = rowIdx;
+                                contentsScope.$column = columnIdx;
+                                var contentsLength = contents.length;
+                                for (var contentIdx = 0; contentIdx < contentsLength; ++contentIdx) {
+                                    var content = contents[contentIdx];
+                                    cell.appendChild(content);
+                                }
+                            });
+                        };
+                    };
+                }
+            };
+        },
         controller: ['$scope', 'calendarService', function ($scope, calendarService) {
-            $scope.columns = [0, 1, 2, 3, 4, 5, 6];
-            $scope.rows = [0, 1, 2, 3, 4];
-
             $scope.dayNames = calendarService.shortDayNames;
 
             var monthNames = calendarService.longMonthNames;
