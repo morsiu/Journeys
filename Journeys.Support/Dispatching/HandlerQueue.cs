fe8a97b0770dc9b4a7e4cb66782c4e0c6b3be232
Łukasz Mrozek <morskk@gmail.com>
2014-07-09 21:39:03 +0200

    Exposed wait handle on handler queue

--- Journeys.Support/Dispatching/HandlerQueue.cs
+++ Journeys.Support/Dispatching/HandlerQueue.cs
@@ -1,20 +1,45 @@
 ﻿using System;
-using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
 
 namespace Journeys.Support.Dispatching
 {
     public sealed class HandlerQueue
     {
-        private readonly ConcurrentQueue<Action> _queuedHandlers = new ConcurrentQueue<Action>();
+        private readonly Queue<Action> _queuedHandlers = new Queue<Action>();
+        private readonly ManualResetEvent _nonEmptyQueueEvent = new ManualResetEvent(false);
+        private readonly object _accessLock = new object();
+
+        public WaitHandle WaitHandle
+        {
+            get { return _nonEmptyQueueEvent; }
+        }
 
         public void Queue(Action handler)
         {
-            _queuedHandlers.Enqueue(handler);
+            lock (_accessLock)
+            {
+                _queuedHandlers.Enqueue(handler);
+                _nonEmptyQueueEvent.Set();
+            }
         }
 
         public bool TryDequeue(out Action handler)
         {
-            return _queuedHandlers.TryDequeue(out handler);
+            lock (_accessLock)
+            {
+                if (_queuedHandlers.Count == 0)
+                {
+                    handler = null;
+                    return false;
+                }
+                if (_queuedHandlers.Count == 1)
+                {
+                    _nonEmptyQueueEvent.Reset();
+                }
+                handler = _queuedHandlers.Dequeue();
+                return true;
+            }
         }
     }
 }
