883b176463baf3a6ead19235c5327be1a9952ef5
Łukasz Mrozek <morskk@gmail.com>
2014-07-09 22:11:25 +0200

    Added AsyncQuery

---
+++ Journeys.Hosting.Adapters/Dispatching/AsyncQuery.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+using Journeys.Hosting.Adapters.Messages;
+using Journeys.Support.Dispatching;
+
+namespace Journeys.Hosting.Adapters.Dispatching
+{
+    public sealed class AsyncQuery
+    {
+        private readonly object _querySpecification;
+        private readonly HandlerRegistry _registry;
+
+        public AsyncQuery(object querySpecification, HandlerRegistry registry)
+        {
+            _querySpecification = querySpecification;
+            _registry = registry;
+        }
+
+        public Task<object> Execute(HandlerQueue queue)
+        {
+            var handler = FindHandler();
+            var resultSource = new TaskCompletionSource<object>();
+            queue.Enqueue(
+                () =>
+                {
+                    try
+                    {
+                        resultSource.SetResult(handler(_querySpecification));
+                    }
+                    catch (Exception e)
+                    {
+                        resultSource.SetException(e);
+                    }
+                });
+            return resultSource.Task;
+        }
+
+        private Func<object, object> FindHandler()
+        {
+            var queryKey = new QueryKey(_querySpecification.GetType());
+            Func<object, object> handler;
+            if (_registry.Retrieve(queryKey, out handler))
+            {
+                return handler;
+            }
+            throw new InvalidOperationException(string.Format(FailureMessages.NoHandlerRegisteredForQueryOfType, queryKey));
+        }
+    }
+}
