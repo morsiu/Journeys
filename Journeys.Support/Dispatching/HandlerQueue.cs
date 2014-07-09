8b79ccf7c5d210464ae19fc942b1ee3b7748a3e8
Łukasz Mrozek <morskk@gmail.com>
2014-07-09 20:15:32 +0200

    Added handler queue

---
+++ Journeys.Support/Dispatching/HandlerQueue.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace Journeys.Support.Dispatching
+{
+    public sealed class HandlerQueue
+    {
+        private readonly ConcurrentQueue<Action> _queuedHandlers = new ConcurrentQueue<Action>();
+
+        public void Queue(Action handler)
+        {
+            _queuedHandlers.Enqueue(handler);
+        }
+
+        public bool TryDequeue(out Action handler)
+        {
+            return _queuedHandlers.TryDequeue(out handler);
+        }
+    }
+}
