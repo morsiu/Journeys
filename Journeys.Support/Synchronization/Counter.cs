1e8ab07cec9a5e1052b4dd2be84a3a12584c1244
Łukasz Mrozek <morskk@gmail.com>
2014-07-09 23:18:48 +0200

    Added asynchronous handler dispatcher, which uses queue for dispatching. Added Counter synchronization primitive, which is signaled when 0.

---
+++ Journeys.Support/Synchronization/Counter.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading;
+
+namespace Journeys.Support.Synchronization
+{
+    public sealed class Counter
+    {
+        private int _count;
+        private EventWaitHandle _event;
+
+        public Counter(int initialCount)
+        {
+            _count = initialCount;
+            _event = new ManualResetEvent(true);
+        }
+
+        public void Increase()
+        {
+            if (Interlocked.Increment(ref _count) == 1)
+            {
+                _event.Reset();
+            }
+        }
+
+        public void Decrease()
+        {
+            if (Interlocked.Decrement(ref _count) == 0)
+            {
+                _event.Set();
+            }
+        }
+
+        public void Wait()
+        {
+            _event.WaitOne();
+        }
+    }
+}
