9a79f3f7777280ef876018b32cd5755a8e437591
Łukasz Mrozek <morskk@gmail.com>
2014-07-09 23:19:27 +0200

    Removed dispatching responsibility from AsyncQuery by using AsyncHandlerDispatcher

--- Journeys.Hosting.Adapters/Dispatching/AsyncQuery.cs
+++ Journeys.Hosting.Adapters/Dispatching/AsyncQuery.cs
@@ -2,48 +2,30 @@
 using System.Threading.Tasks;
 using Journeys.Hosting.Adapters.Messages;
 using Journeys.Support.Dispatching;
+using Journeys.Support.Dispatching.Exceptions;
 
 namespace Journeys.Hosting.Adapters.Dispatching
 {
     public sealed class AsyncQuery
     {
         private readonly object _querySpecification;
-        private readonly HandlerRegistry _registry;
 
-        public AsyncQuery(object querySpecification, HandlerRegistry registry)
+        public AsyncQuery(object querySpecification)
         {
             _querySpecification = querySpecification;
-            _registry = registry;
         }
 
-        public Task<object> Execute(HandlerQueue queue)
-        {
-            var handler = FindHandler();
-            var resultSource = new TaskCompletionSource<object>();
-            queue.Enqueue(
-                () =>
-                {
-                    try
-                    {
-                        resultSource.SetResult(handler(_querySpecification));
-                    }
-                    catch (Exception e)
-                    {
-                        resultSource.SetException(e);
-                    }
-                });
-            return resultSource.Task;
-        }
-
-        private Func<object, object> FindHandler()
+        public Task<object> Execute(AsyncHandlerDispatcher dispatcher)
         {
             var queryKey = new QueryKey(_querySpecification.GetType());
-            Func<object, object> handler;
-            if (_registry.Retrieve(queryKey, out handler))
+            try
             {
-                return handler;
+                return dispatcher.Dispatch(queryKey, _querySpecification);
             }
-            throw new InvalidOperationException(string.Format(FailureMessages.NoHandlerRegisteredForQueryOfType, queryKey));
+            catch (HandlerNotFoundException)
+            {
+                throw new InvalidOperationException(string.Format(FailureMessages.NoHandlerRegisteredForQueryOfType, queryKey));
+            }
         }
     }
 }
