bdfb6115a170ea3deecb2da7fd97358dfef65f40
Łukasz Mrozek <morskk@gmail.com>
2014-10-19 10:56:25 +0200

    Added company information.

--- Mors.Journeys.Application/Properties/AssemblyInfo.cs
+++ Mors.Journeys.Application/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Mors.Journeys.Application.Command")]
+[assembly: AssemblyTitle("Mors.Journeys.Application")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Mors.Journeys.Application.Command")]
+[assembly: AssemblyCompany("Łukasz Mrozek")]
+[assembly: AssemblyProduct("Mors.Journeys.Application")]
 [assembly: AssemblyCopyright("Copyright © Łukasz Mrozek 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
