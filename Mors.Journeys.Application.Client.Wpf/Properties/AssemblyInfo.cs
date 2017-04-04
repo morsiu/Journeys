7b6503dc523558a3d81405e6c46afe7f21f6d17d
Łukasz Mrozek <morskk@gmail.com>
2017-04-04 19:45:19 +0200

    Bumped version of the desktop client to 3.0.0-alpha1

--- Mors.Journeys.Application.Client.Wpf/Properties/AssemblyInfo.cs
+++ Mors.Journeys.Application.Client.Wpf/Properties/AssemblyInfo.cs
@@ -51,3 +51,4 @@ using System.Windows;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.0.0.0")]
 [assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyInformationalVersion("3.0.0-alpha1")]
